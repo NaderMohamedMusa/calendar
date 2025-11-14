c16ac250631d403d4d858a6db98aca682bf3ce8e
NaderMohamed <mo.nader298@gmail.com>
2025-11-15 01:17:56 +0200

    chorn: finished task

--- lib/features/calendar/presentation/view/widgets/calendar_card.dart
+++ lib/features/calendar/presentation/view/widgets/calendar_card.dart
@@ -21,7 +21,7 @@ class CalendarCard extends StatelessWidget {
               lastDay: DateTime(2100),
               pageJumpingEnabled: true,
               pageAnimationEnabled: true,
-              daysOfWeekVisible: true,
+              daysOfWeekVisible: !state.isWeek,
               headerVisible: !state.isWeek,
               calendarFormat: state.isWeek
                   ? CalendarFormat.week
@@ -52,10 +52,99 @@ class CalendarCard extends StatelessWidget {
                   color: Color(0xFF22BDB7),
                   shape: BoxShape.circle,
                 ),
+
                 markersMaxCount: 3,
               ),
+              calendarBuilders: state.isWeek
+                  ? CalendarBuilders(
+                selectedBuilder: (context,
+                    date, _) {
+                  final isWeek = context.read<CalendarCubit>().state.isWeek;
+
+                  if (!isWeek) return null;
+
+                  final isSelected = isSameDay(
+                    date,
+                    context.read<CalendarCubit>().state.selectedDay,
+                  );
+
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: isSelected
+                          ? const Color(0xFF22BDB7)
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          _getDayNameShort(date),
+                          style: TextStyle(
+                            color: isSelected ? Colors.white : Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 14),
+                          decoration: BoxDecoration(
+                            color: isSelected
+                                ? const Color(0xFF22BDB7)
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            date.day.toString(),
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.black87,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                defaultBuilder: (context,
+                    date, _) =>Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          _getDayNameShort(date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 14),
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            date.day.toString(),
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+              )
+                  :  CalendarBuilders() ,
+
               eventLoader: (day) {
-                if ([5, 12, 14, 19, 24].contains(day.day)) return ['x'];
                 return const <String>[];
               },
             ),
@@ -64,4 +153,18 @@ class CalendarCard extends StatelessWidget {
       },
     );
   }
+
+  String _getDayNameShort(DateTime date) {
+    const names = [
+      'سبت',
+      'أحد',
+      'اثنين',
+      'ثلاثاء',
+      'أربعاء',
+      'خميس',
+      'جمعة',
+    ];
+    return names[date.weekday % 7];
+  }
+
 }
